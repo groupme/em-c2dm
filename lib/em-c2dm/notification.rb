2a7db0be1b7d218b8b9ae6ba1e3d9994bcd65b82
Brandon Keene <bkeene@gmail.com>
2011-06-27 17:38:05 -0400

    stringify keys

--- lib/em-c2dm/notification.rb
+++ lib/em-c2dm/notification.rb
@@ -17,7 +17,7 @@ module EventMachine
 
       def generate_params
         params = { "registration_id" => @registration_id }
-        params["collapse_key"] = @options.delete(:collapse_key) if @options[:collapse_key]
+        params["collapse_key"] = @options.delete("collapse_key") if @options["collapse_key"]
         @options.each { |k,v| params["data.#{k}"] = v }
         params
       end
