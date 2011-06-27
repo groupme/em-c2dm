1651c04c1742c16934be34208749d35c49e27988
Brandon Keene <bkeene@gmail.com>
2011-06-27 18:58:44 -0400

    debug

--- lib/em-c2dm/notification.rb
+++ lib/em-c2dm/notification.rb
@@ -5,6 +5,7 @@ module EventMachine
 
       def initialize(registration_id, options = {})
         @registration_id, @options = registration_id, options
+        puts options.inspect
         raise ArgumentError.new("missing options") if options.nil? || options.empty?
         @uuid = $uuid.generate
       end
@@ -21,6 +22,7 @@ module EventMachine
         @options.each do |k,v|
           params["data.#{k}"] = v
         end
+        puts params.inspect
         params
       end
     end
